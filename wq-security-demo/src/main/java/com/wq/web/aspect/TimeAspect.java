3d137dd63c0eed9ec4750fd923f77a948a223316
wuqingvika <526288022@qq.com>
2018-12-08 22:07:59 +0800

    切面

---
+++ wq-security-demo/src/main/java/com/wq/web/aspect/TimeAspect.java
@@ -0,0 +1,36 @@
+package com.wq.web.aspect;
+
+import java.util.Date;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class TimeAspect {
+	//具体定义见https://docs.spring.io/spring/docs/5.1.3.RELEASE/spring-framework-reference/core.html#aop-pointcuts
+	@Around("execution(* com.wq.web.controller.UserController.*(..))")
+	public Object handleControllerMethod(ProceedingJoinPoint pjp) throws Throwable {
+		
+		System.out.println("time aspect start");
+		
+		Object[] args = pjp.getArgs();
+		for (Object arg : args) {
+			System.out.println("arg is "+arg);
+		}
+		
+		long start = new Date().getTime();
+		
+		Object object = pjp.proceed();
+		
+		System.out.println("time aspect 耗时:"+ (new Date().getTime() - start));
+		
+		System.out.println("time aspect end");
+		
+		return object;
+	}
+
+}
