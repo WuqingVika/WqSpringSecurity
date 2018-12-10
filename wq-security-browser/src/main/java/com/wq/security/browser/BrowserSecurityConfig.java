c116f03926c267ba7c3d1facdec7b4bdf50c97b2
wuqingvika <526288022@qq.com>
2018-12-10 15:45:51 +0800

    14.自定义用户认证逻辑 add on 20181210

--- wq-security-browser/src/main/java/com/wq/security/browser/BrowserSecurityConfig.java
+++ wq-security-browser/src/main/java/com/wq/security/browser/BrowserSecurityConfig.java
@@ -1,20 +1,29 @@
 package com.wq.security.browser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by qwu on 2018/12/10.
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();//这里也可以用自定义的加密的(只要实现加密解密方法就行) 或者Md5
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //1.SpringSecurity默认的登录方式
         //   1.1 通过表单登录的方式
-        /*http.formLogin().and().authorizeRequests().anyRequest().authenticated();*/
+        http.formLogin().and().authorizeRequests().anyRequest().authenticated();
         //   1.2 通过弹出式登录
-        http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
+        //http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
     }
 }
